378c640b1e7e1371d12397cb229f45c345ec9daf
Ryan Roemer <ryan.roemer@formidable.com>
2020-03-10 13:58:13 -0700

    Add all the packages.

---
+++ packages/cart/src/index.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import htm from "htm";
+const html = htm.bind(React.createElement);
+
+const App = () => html `<h1>Cart</h1>`;
+
+ReactDOM.render(
+  html `<${App} />`,
+  document.getElementById("root")
+);
