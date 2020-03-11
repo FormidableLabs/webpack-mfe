7259580f9f41e5e35004a2422c9dc12b1010a45c
Ryan Roemer <ryan.roemer@formidable.com>
2020-03-11 14:02:52 -0700

    Move data down to item component

--- packages/item/src/components/item.js
+++ packages/item/src/components/item.js
@@ -1,14 +1,48 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import htm from "htm";
 const html = htm.bind(React.createElement);
 
-export const Item = ({ id, name, emoji }) => html `
+// TODO: Abstract this (or a memo fn).
+const _data = () => {
+  _data.prom = _data.prom || import("app_homepage/data/index");
+  return _data.prom;
+};
+
+const Loading = ({ id }) => html `
+  <div style=${{ textAlign: "center" }} className="pure-u-1-3">
+    <em style=${{ fontSize: "1.5em", lineHeight: "2em" }}>Loading item ${id}...</em>
+  </div>
+`;
+
+export const Item = ({ id, name, emoji }) => {
+  const idNum = parseInt(id, 10);
+  const location = useLocation();
+  const [item, setData] = React.useState({ id, name, emoji });
+
+  React.useEffect(() => {
+    // Skip if already have data for current element.
+    if (item.name && item.id === idNum) {
+      return;
+    }
+
+    _data()
+      .then(({ fetchItem }) => fetchItem({ id }))
+      .then((d) => setData(d))
+      .catch(() => {});
+  }, [item, location]);
+
+  // Wait for data load.
+  if (!item.name || item.id !== idNum) {
+    return html `<${Loading} id=${id} />`;
+  }
+
+  return html `
     <${Link}
       to=${{
-    pathname: `/item/${id}`,
-    state: { item: { id, name, emoji } }
+    pathname: `/item/${item.id}`,
+    state: { item }
   }}
       style=${{
     color: "inherit",
@@ -18,7 +52,8 @@ export const Item = ({ id, name, emoji }) => html `
   }}
       className="pure-u-1-3"
     >
-      <p style=${{ fontSize: "10em" }}>${emoji}</p>
-      <p style=${{ fontSize: "2em" }}>${name}</p>
+      <p style=${{ fontSize: "10em" }}>${item.emoji}</p>
+      <p style=${{ fontSize: "2em" }}>${item.name}</p>
     </${Link}>
   `;
+};
--- packages/item/src/pages/item.js
+++ packages/item/src/pages/item.js
@@ -1,16 +1,9 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
 
 import { Item } from "../components/item";
 
 import htm from "htm";
 const html = htm.bind(React.createElement);
-
-// TODO: Abstract this (or a memo fn).
-const _data = () => {
-  _data.prom = _data.prom || import("app_homepage/data/index");
-  return _data.prom;
-};
 
 // ----------------------------------------------------------------------------
 // Shared components
@@ -20,36 +13,14 @@ const Page = React.lazy(() => import("app_homepage/components/page"));
 // ----------------------------------------------------------------------------
 // Components
 // ----------------------------------------------------------------------------
-const Loading = ({ id }) => html `
-  <div style=${{ textAlign: "center" }} className="pure-u-1-3">
-    <em style=${{ fontSize: "1.5em", lineHeight: "2em" }}>Loading item ${id}...</em>
-  </div>
-`;
-
 const ItemPage = ({ location, match }) => {
-  // Props.
   const item = ((location || {}).state || {}).item;
   const id = ((match || {}).params || {}).id;
-
-  // Hooks.
-  const loc = useLocation();
-  const [data, setData] = React.useState(item);
-  React.useEffect(() => {
-    // Skip if already have data for current element.
-    if (data && data.id === parseInt(id, 10)) {
-      return;
-    }
-
-    _data()
-      .then(({ fetchItem }) => fetchItem({ id }))
-      .then((d) => setData(d))
-      .catch(() => {});
-  }, [loc]);
 
   return html `
     <${Page} name="Item">
       <div className="pure-u-1-3"></div>
-      ${data ? html `<${Item} ...${data} />` : html `<${Loading} id=${id} />`}
+      <${Item} ...${item || { id }} />
       <div className="pure-u-1-3"></div>
     </${Page}>
   `;
