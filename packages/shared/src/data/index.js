f06cc6bfa760eb2ab0b330e50cfe02ab116ba572
Ryan Roemer <ryan.roemer@formidable.com>
2021-09-07 16:14:06 -0700

    Implement hacked up random

--- packages/shared/src/data/index.js
+++ packages/shared/src/data/index.js
@@ -37,20 +37,42 @@ export const fetchItem = ({ id, cache = true }) => {
     });
 };
 
-// HACK: Number of pages as of 9/7/2021 for `limit=9`.
-const NUM_EMOJIS = 8944;
-const NUM_PER_PAGE = 9;
-const NUM_PAGES = NUM_EMOJIS / NUM_PER_PAGE;
+// HACK: As of 9/7/2021 there are 8944 emojis, indexed by popularity.
+// To provide "better" emojis, we limit to first 1000.
+// We'd also like to only limit to only the 9 we need, but many times
+// you will get null emojis, so try to use non-null first.
+const NUM_EMOJIS_TOTAL = 1000;
+const PER_PAGE_MULTIPLIER = 3;
 
-const _getRandomPage = () => Math.floor(Math.random() * NUM_PAGES);
+const _getRandomPage = (num) =>
+  Math.floor(Math.random() * (NUM_EMOJIS_TOTAL / (num * PER_PAGE_MULTIPLIER)));
 
 // Emojidex doesn't have a random endpoint, so we just do a capped query at 9
 // emojis fetching a random page through all the data.
-export const fetchRandomItems = () => _fetchEmoji({
-  query: `emoji?limit=9&page=${_getRandomPage()}`
+export const fetchRandomItems = (num) => _fetchEmoji({
+  query: `emoji?limit=${num * PER_PAGE_MULTIPLIER}&page=${_getRandomPage(num)}`
 })
-  .then((data) => (data.emoji || []).map(_normalizeData))
-  .then((d) => {
-    console.log("TODO RANDOM", { d });
-    return d;
+  .then(({ emoji = [] } = {}) => {
+    // Iterate and fill arrays in priority order (ascii + emoji first)
+    const ascii = [];
+    const nonAscii = [];
+    const noEmoji = [];
+
+    emoji.forEach((data) => {
+      if (data.moji) {
+        // eslint-disable-next-line no-control-regex
+        if ((/^[\x00-\x7F]*$/).test(data.code)) {
+          ascii.push(data);
+        } else {
+          nonAscii.push(data);
+        }
+      } else {
+        noEmoji.push(data);
+      }
+    });
+
+    return []
+      .concat(ascii, nonAscii, noEmoji)
+      .slice(0, num)
+      .map(_normalizeData);
   });
